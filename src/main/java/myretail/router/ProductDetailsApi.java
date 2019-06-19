15c9bf168b36f82561fbe54a6ea85c23a84fc610
Siddarththiyagarajan <siddarththiyagarajan105@gmail.com>
2019-06-19 17:50:09 +0530

    adding WebFluxConfigurer

--- src/main/java/myretail/router/ProductDetailsApi.java
+++ src/main/java/myretail/router/ProductDetailsApi.java
@@ -4,6 +4,9 @@ import myretail.handler.ProductDetailsApiHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -12,7 +15,8 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
-public class ProductDetailsApi {
+//@EnableWebFlux
+public class ProductDetailsApi { //implements WebFluxConfigurer
 
     private ProductDetailsApiHandler productDetailsApiHandler;
 
@@ -27,5 +31,13 @@ public class ProductDetailsApi {
                 .andRoute(POST("/rest/products/{id}"), productDetailsApiHandler :: createProductDetails)
                 .andRoute(POST("/rest/createMultipleProducts"), productDetailsApiHandler :: createProducts);
     }
+
+    /*@Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://allowed-origin.com")
+                .allowedMethods("PUT")
+                .maxAge(3600);
+    }*/
 
 }
